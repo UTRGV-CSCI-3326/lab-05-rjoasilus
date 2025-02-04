8a359cbeedd3297a35eb200398934595f30e20e4
Ricardo Joasilus <rjoasilus@hotmail.com>
2025-02-04 04:17:03 +0000

    Assignment 5 - Input

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+public class Input
+{
+    public static void main(String[] argv)
+    {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+        String userName = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int userAge = scanner.nextInt();
+
+        System.out.print("Enter your weight in pound: ");
+        double userWeight = scanner.nextDouble();
+
+        System.out.print("Are you a smoker? true/false: ");
+        boolean userDecision = scanner.nextBoolean();
+
+        System.out.println();
+        System.out.println("Your name is: " + userName);
+        System.out.println("Your age is: " + userAge);
+        System.out.println("Your weight is: " + userWeight);
+        System.out.println("Smoker: " + userDecision);
+
+        scanner.close();
+    }
+}
